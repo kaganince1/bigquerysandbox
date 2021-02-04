c6b025ced3e69f392ef79b008dd0496182b8bc2b
kaganince1 <71889416+kaganince1@users.noreply.github.com>
2021-02-04 18:27:28 +0300

    allFiles

---
+++ BigDataProject_/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace BigDataProject_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtboxQuery = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 456);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtboxQuery
+            // 
+            this.txtboxQuery.Location = new System.Drawing.Point(40, 14);
+            this.txtboxQuery.Name = "txtboxQuery";
+            this.txtboxQuery.Size = new System.Drawing.Size(62, 20);
+            this.txtboxQuery.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(108, 12);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 25);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Get";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Title:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(679, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Get";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Genres:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(504, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 511);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxQuery);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "BigQuery";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtboxQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ BigDataProject_/Form1.cs
@@ -0,0 +1,103 @@
+﻿using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+using Google.Cloud.BigQuery.V2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigDataProject_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void WriteRows(IList<TableRow> rows, IList<TableFieldSchema> fields)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (txtboxQuery.Text.Length == 0)
+            {
+                System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "D:/İndirilenler/key.json");
+                var client = BigQueryClient.Create("molten-method-300611");
+                var table = client.GetTable("molten-method-300611", "deneme", "movies");
+                var sql = $"SELECT * FROM `molten-method-300611.deneme.movies` order by movieId asc limit 100";
+                var results = client.ExecuteQuery(sql, parameters: null);
+                List<string> deneme = new List<string>();
+                foreach (var row in results)
+                {
+                    deneme.Add(($"ID: {row["movieId"]} Title: {row["title"]} Genres: {row["genres"]}"));
+                }
+                var result = deneme.Select(s => new { value = s }).ToList();
+                dataGridView1.DataSource = result;
+            }
+            else
+            {
+                System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "D:/İndirilenler/key.json");
+                var client = BigQueryClient.Create("molten-method-300611");
+                var table = client.GetTable("molten-method-300611", "deneme", "movies");
+                string query = "SELECT * FROM `molten-method-300611.deneme.movies` where movieId = "+int.Parse(txtboxQuery.Text)+" order by movieId asc limit 100";
+                var sql = query;
+                var results = client.ExecuteQuery(sql, parameters: null);
+                List<string> deneme = new List<string>();
+                foreach (var row in results)
+                {
+                    deneme.Add(($"ID: {row["movieId"]} Title: {row["title"]} Genres: {row["genres"]}"));
+                }
+                var result = deneme.Select(s => new { value = s }).ToList();
+                dataGridView1.DataSource = result;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "D:/İndirilenler/key.json");
+            var client = BigQueryClient.Create("molten-method-300611");
+            var table = client.GetTable("molten-method-300611", "deneme", "movies");
+            string query = "SELECT * FROM `molten-method-300611.deneme.movies` where title like '%" + textBox1.Text + "%' limit 100";
+            var sql = query;
+            var results = client.ExecuteQuery(sql, parameters: null);
+            List<string> deneme = new List<string>();
+            foreach (var row in results)
+            {
+                deneme.Add(($"ID: {row["movieId"]} Title: {row["title"]} Genres: {row["genres"]}"));
+            }
+            var result = deneme.Select(s => new { value = s }).ToList();
+            dataGridView1.DataSource = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "D:/İndirilenler/key.json");
+            var client = BigQueryClient.Create("molten-method-300611");
+            var table = client.GetTable("molten-method-300611", "deneme", "movies");
+            string query = "SELECT * FROM `molten-method-300611.deneme.movies` where genres like '%" + textBox2.Text + "%' limit 100";
+            var sql = query;
+            var results = client.ExecuteQuery(sql, parameters: null);
+            List<string> deneme = new List<string>();
+            foreach (var row in results)
+            {
+                deneme.Add(($"ID: {row["movieId"]} Title: {row["title"]} Genres: {row["genres"]}"));
+            }
+            var result = deneme.Select(s => new { value = s }).ToList();
+            dataGridView1.DataSource = result;
+        }
+    }
+}
